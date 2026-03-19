bf04eba85f46a2505c33e69751c1879e4feab714
igornunescnt <igornunescnt@gmail.com>
2026-03-19 16:57:04 -0300

    second commit

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -2,13 +2,20 @@ import Logo from '../componentes/Logo';
 import Menu from '../componentes/Menu';
 import IconesHeader from '../componentes/IconesHeader';
 import './Header.css';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+    align-items: center;
+` 
 
 export default function Header() {
   return (
-        <header className="App-header">
+        <HeaderContainer>
             <Logo />
             <Menu />
             <IconesHeader />
-        </header>
+        </HeaderContainer>
   )
 }
